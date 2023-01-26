97254da4b1f0715bdffd4da985e06acb17fe3f6f
João Figueredo <joaoklvf@gmail.com>
2023-01-25 22:58:35 -0300

    Removing post stuff

--- src/repository/post.repository.ts
+++
@@ -1,8 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { PostEntity } from "../database/entities/post.entity";
-
-@EntityRepository(PostEntity)
-
-export class PostRepository extends Repository<PostEntity> {
-
-}
