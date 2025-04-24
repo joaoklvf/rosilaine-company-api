fb4d9f62466bd9de0f45a46dc45b4c69f4db0263
João Figueredo <joaoklvf@gmail.com>
2025-04-24 17:58:47 -0300

    feat: IRepoService create possibly null

--- src/interfaces/repo-service.ts
+++ src/interfaces/repo-service.ts
@@ -7,7 +7,7 @@ import { DeleteResult } from "typeorm";
 export interface IRepoService<T> {          // Exportado
   delete(id: string): Promise<DeleteResult>;
   index(): Promise<T[]>;
-  create(customer: T): Promise<T>;
+  create(customer: T): Promise<T | null>;
   update(customer: T, id: string): Promise<T | null>;
   get(id: string): Promise<T | null>
 }
