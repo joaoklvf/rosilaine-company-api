f22e2a2103b91d7e01bcf7671de7427b76bb8e25
João Figueredo <joaoklvf@gmail.com>
2025-06-09 19:37:00 -0300

    teste: db connection

--- api/index.ts
+++ api/index.ts
@@ -12,9 +12,16 @@ const app = express();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [`${__dirname}/../**/*.entity.{js,ts}`],
-  url: process.env.DATABASE_URL
+  synchronize: true,
+  name: process.env.DATABASE_NAME
 });
+
 app.use(cors());
 app.use(express.json());
 app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
